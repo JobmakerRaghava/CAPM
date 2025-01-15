22129ad1b9b08f095e6f26f4ddc9d3249feabed1
JobmakerRaghava <123930030+JobmakerRaghava@users.noreply.github.com>
2025-01-15 16:35:36 +0530

    Add files via upload

---
+++ tagamodelui/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("tagamodelui.tagamodelui.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ tagamodelui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["tagamodelui/tagamodelui/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
